3742cbadd6aad51f23c7d8c7b63d94f442420094
RickPistola <ronnyguilherme.silva@outlook.com>
2024-11-27 18:04:54 -0300

    tentativas ate acertar

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,11 +1,30 @@
 alert('Boas Vindas ao jogo do número secreto');
 let numeroSecreto = 29;
-console.log(numeroSecreto)
-let chute = prompt('Escolha um número entre 1 e 30');
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
 
-// se o chute for igual ao numero secreto
-if (numeroSecreto == chute){
-    alert(`isso ai! você descubriu o número ${numeroSecreto}`);
-}else{
-    alert('você errou')
+
+// enquanto chute não for igual
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 30');
+    // se o chute for igual ao numero secreto
+    if (numeroSecreto == chute){
+        break;
+    }else{
+        if (chute > numeroSecreto){
+            alert(`o número secreto é menor que ${chute}`);
+        } else{
+            alert(`o número secreto é maior que ${chute}`);
+        }
+        // tentativas = tentativas + 1;
+        tentativas++;
+    }
 }
+if (tentativas > 1){
+    alert(`isso ai! você descubriu o número ${numeroSecreto} com ${tentativas} tentativas`);
+}else{
+    alert(`isso ai! você descubriu o número ${numeroSecreto} com ${tentativas} tentativa`);
+}
+
+
